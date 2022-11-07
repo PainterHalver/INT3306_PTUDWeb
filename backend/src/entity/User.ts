9a8a209fc5a6fa51809c7683592b3ba68be174bd
painterhalver <hieptrang13042002@gmail.com>
2022-11-07 22:48:32 +0700

    Thêm Schema Customer

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -7,17 +7,17 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ nullable: false })
   name: string;
 
   @Length(3, 255, { message: "Username cần phải có trên 3 ký tự" })
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: false })
   username: string;
 
   @Exclude()
-  @Column()
+  @Column({ nullable: false })
   password: string;
 
-  @Column()
-  account_type: "admin" | "san_xuat" | "dai_ly" | "bao_hanh";
+  @Column({ nullable: false })
+  account_type: "admin" | "saan_xuat" | "dai_ly" | "bao_hanh";
 }
