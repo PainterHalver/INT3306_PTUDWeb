21b79ec3b811779a08f25172cd67236f05934b97
painterhalver <hieptrang13042002@gmail.com>
2022-11-12 16:12:11 +0700

    Route thêm customer cho đại lý

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -37,8 +37,48 @@ const getCustomers = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Tạo một khách hàng mới.
+ * Đại lý tạo khách hàng mới khi khách hàng đến mua hàng lần đầu.
+ */
+const createCustomer = async (req: Request, res: Response) => {
+  try {
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const address = req.body.address || "";
+
+    // Validate inputs
+    let errors: any = {};
+    if (!name || typeof name !== "string") errors.name = "Tên khách hàng không được để trống";
+    if (!phone || typeof phone !== "string") errors.phone = "Số điện thoại không được để trống";
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    // Kiểm tra số điện thoại đã tồn tại chưa
+    const phoneCustomer = await customerRepo.findOneBy({ phone });
+    if (phoneCustomer) {
+      return res.status(400).json({ phone: "Số điện thoại đã tồn tại" });
+    }
+
+    // Tạo khách hàng mới
+    const customer = customerRepo.create({
+      name,
+      phone,
+      address,
+    });
+    const savedCustomer = await customerRepo.save(customer);
+
+    // Trả về thông tin khách hàng mới
+    return res.status(201).json(savedCustomer);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+};
+
 const router = Router();
 
 router.get("/", protectRoute, restrictTo("admin", "dai_ly", "bao_hanh", "san_xuat"), getCustomers);
+router.post("/", protectRoute, restrictTo("dai_ly"), createCustomer);
 
 export default router;
